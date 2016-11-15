e830682ad6d5a2063fe1567a7da4940577bca4b7
Paul Jaworski <pjaworski27@gmail.com>
2016-11-15 11:11:08 -0800

    basic work history filler

---
+++ src/components/WorkHistory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WorkHistory = () => {
+	return (
+		<div className="work-history">
+			<h3>Things I've worked on...</h3>
+		</div>
+	);
+};
+
+export default WorkHistory;
